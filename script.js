efed01f6e941f9ed14580426f12661484b02e378
Nikola <gedovicc96@gmail.com>
2023-10-06 15:59:15 +0200

    validation of input script.js

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
+const options = ["rock", "paper", "scissors"];
+
+// Play a Round Function
+
+function game() {
+    playRound();
+}
+
 // Computers Choice Function
 function getComputerChoice() {
-    let options = ["rock", "paper", "scissors"];
-    let choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
+    let choices = options[Math.floor(Math.random() * options.length)];
+    return choices;
 }
 
 // console.log(getComputerChoice());
 
 // User Choice Function
 function getUserChoice() {
-    let userSelection = prompt("Type Rock, Paper or Scissors.");
-    let option = userSelection.toLowerCase();
-    return option;
+    let input = prompt("Type Rock, Paper or Scissors.");
+    while (input == null) {
+        input = prompt("Type Rock, Paper or Scissors.")
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    if (check == true) {
+        console.log(input);
+    }
+    return input;
 }
 
+function playRound() {
+    const playerSelection = getUserChoice();
+    const computerSelection = getComputerChoice();
+}
+
+// Validate Input Function
+function validateInput(choice) {
+    if (options.includes(choice)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+game();
