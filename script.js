bbc46fab3befb3df6d5aad4359e47798a5ab038b
Nikola <gedovicc96@gmail.com>
2023-10-06 15:41:49 +0200

    add player choice in js script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Computers Choice Function
 function getComputerChoice() {
-    let options = ["Rock", "Paper", "Scissors"];
+    let options = ["rock", "paper", "scissors"];
     let choice = options[Math.floor(Math.random() * options.length)];
     return choice;
+}
+
+// console.log(getComputerChoice());
+
+// User Choice Function
+function getUserChoice() {
+    let userSelection = prompt("Type Rock, Paper or Scissors.");
+    let option = userSelection.toLowerCase();
+    return option;
 }
 
